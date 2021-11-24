f891f6da85a70da2d431a05e07eaa4163eaeb960
biud436 <biud436@gmail.com>
2021-11-25 00:19:32 +0900

    필터링 함수 수정

--- project/__main__.py
+++ project/__main__.py
@@ -4,16 +4,20 @@ from parser.OptionParserBuilder import OptionParserBuilder
 class EntryPoint:
     @staticmethod
     def main():
+        # 옵션 파서 생성
         args = OptionParserBuilder.build()
-
         font_name = args.font.strip()
+
+        # 폰트 파싱 객체 생성
         font = Font(font_name or "나눔고딕")
         font.parse()
 
-        filter(lambda x: x.language_id == 23, font.fonts)
-
+        # 필터링
+        results = filter(lambda x: x.language_id == 1033 or x.language_id == 1042, font.fonts)
+        
+        # 폰트 파싱 완료
         print("\x1b[1;32m" + "[폰트명 파싱 완료]" + "\x1b[0m")
-        for font in font.fonts:
+        for font in list(results):
             print("{font_name} [{language_id}]".format(font_name=font.name, language_id = font.language_id))
 
 
