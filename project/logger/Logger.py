1e548a5feb63d1cbc94e09be9bc1e35ac87b9bad
biud436 <biud436@gmail.com>
2021-11-25 23:21:42 +0900

    EventEmitter를 상속....

--- project/logger/Logger.py
+++ project/logger/Logger.py
@@ -1,8 +1,7 @@
 from pyee import EventEmitter
 
-class Logger(object):
+class Logger(EventEmitter):
     DEBUG_FLAG = False
-    emitter = EventEmitter()
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
@@ -31,24 +30,6 @@ class Logger(object):
 
         print(color + kwargs['message'] + "\x1b[0m")
 
-    def get_log(self):
-        return self.log
-
-    def on(self, event, callback):
-        Logger.emitter.on(event, callback)
-
-    def emit(self, event, *args, **kwargs):
-        Logger.emitter.emit(event, *args, **kwargs)
-
-    def off(self, event, callback):
-        Logger.emitter.off(event, callback)
-    
-    def once(self, event, callback):
-        Logger.emitter.once(event, callback)
-    
-    def removeAllListeners(self, event):
-        Logger.emitter.removeAllListeners(event)
-    
     def debug(self, msg):
         if Logger.DEBUG_FLAG:
             print("\x1b[33m" + msg + "\x1b[0m") 
