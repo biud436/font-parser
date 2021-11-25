d55bf891fae715acc43e27831a55528a09647684
biud436 <biud436@gmail.com>
2021-11-25 14:50:27 +0900

    fixed the Pipfile

--- project/__main__.py
+++ project/__main__.py
@@ -1,5 +1,6 @@
 from font.Font import Font
 from parser.OptionParserBuilder import OptionParserBuilder
+import platform
 
 class EntryPoint:
     @staticmethod
@@ -16,6 +17,10 @@ class EntryPoint:
         results = filter(lambda x: x.language_id == 1033 or x.language_id == 1042, font.fonts)
         
         # 폰트 파싱 완료
+
+        if platform.system().lower().find("windows") != -1:
+            print("윈도우즈")
+
         print("\x1b[1;32m" + "[폰트명 파싱 완료]" + "\x1b[0m")
         for font in list(results):
             print("{font_name} [{language_id}]".format(font_name=font.name, language_id = font.language_id))
